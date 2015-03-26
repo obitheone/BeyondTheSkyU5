d3c5edac161dc36f5fef84dba670b12e23ee699a
Sergio <sergi79@gmail.com>
2015-03-26 17:50:28 +0100

    modelos texturizados

---
+++ Assets/Mushroomtriger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mushroomtriger : MonoBehaviour {
+
+	private Animator Resizator;
+	// Use this for initialization
+	void Start () {
+		Resizator = GetComponent<Animator>();
+		//Resizator.Play("MushroomResize");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Animator anim = GetComponent<Animator>();
+		if (Input.GetKeyUp (KeyCode.P))
+			Resizator.CrossFade ("MushroomResize", 0f);
+		else if(Input.GetKey(KeyCode.O))
+			Resizator.CrossFade("Idle", 0f);
+	}
+}
